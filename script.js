eb1db874ada7512245528d7a330939e0356a8026
unknown <josecunha0740@digitalcollege.internal>
2025-09-16 09:43:59 -0300

    trabalhando tipos de dados primitivos

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Trabalhando com saída de dados.
+
+console.log("Fui criado no arquivo JavaScript.");
+
+// Tipos de dados primitivos.
+console.log("Hello!"); // string
+console.log(29.5)  // number 
+console.log(true, false); // boolean
+console.log(null); // null
+console.log(); // undefined
+// Tipos de dados primitivos. 
+
