878015a174fd424cfd2366648c2cdf75adfa16bf
unknown <josecunha0740@digitalcollege.internal>
2025-09-23 12:05:59 -0300

    aprendendo estrutura repetida

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
 
- const idade = 28;
+// const temperatura = Number(prompt("Temperatura local atual"))
 
- if (idade > 18){
-  console.log("Você é maior de idade")
- } else {
-     console.log("Você é menor de idade")
- };
+//  if(age >= 65){
+//        alert("Você está na melhor idade")
+//     } else if(age >= 18){
+//        alert("Você é maior de idade")
+//     } else if(age === 17){
+//        alert("Você está banido.")
+//     }
+//  else {
+//     alert("Você é menor de idade")
+// }
+
+// if(temperatura > 35){
+//        alert("Calor extremo")
+//     } else if(temperatura >= 25){
+//        alert("Clima quente")
+//     } else if(temperatura >= 15){
+//        alert("Clima agradável")
+//     } else if(temperatura >= 5){
+//        alert("Clima frio")
+//     } else {
+//     alert("Frio intenso")
+// }
+
+// const color = "red";
+
+// switch(color){
+//   case "green":
+//     alert("Go!");
+//     break;
+//     case "yellow":
+//     alert("Atention!");
+//     break;
+//     case "red":
+//     alert("Stop!");
+//     break;
+//     default:
+//      alert("Invalid color");
+// }
  
+//  for(let i = 0; i <= 10; i++){
+//     console.log(i);
+//  }
+
+//  let i = 0;
+
+//  while(i <= 10){
+//     if(i % 2 === 0){
+//     console.log(i);
+// };
+// i++
+// }
+
+const palavra = prompt("Digite uma palavra")
+
+for(let i  = 1; i <= 5; i++){
+    console.log(palavra);
+}
